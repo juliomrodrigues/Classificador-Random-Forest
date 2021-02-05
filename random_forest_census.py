3e0cac9aab28029b5a6b9a28a5d7fbf5f213bbd0
Júlio Rodrigues <76015049+juliomrodrigues@users.noreply.github.com>
2021-02-05 18:55:55 -0300

    Update random_forest_census.py

--- random_forest_census.py
+++ random_forest_census.py
@@ -1,26 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jan 17 23:17:02 2021
-
 Base de Dados: Dua, D. and Graff, C. (2019). UCI Machine Learning Repository 
 [http://archive.ics.uci.edu/ml]. Irvine, CA: University of California, School 
 of Information and Computer Science.
 """
-
 import pandas
+import numpy as np
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.model_selection import StratifiedKFold
  
 base = pandas.read_csv('census.csv')
  
 atributos = base.iloc[:, 0:14].values
 classe = base.iloc[:, 14].values
 
-# Transformando variaveis categóricas(Strings) em números
+# LABEL_ENCODER
 label_encoder= LabelEncoder()
 atributos[:, 1] = label_encoder.fit_transform(atributos[:, 1])
 atributos[:, 3] = label_encoder.fit_transform(atributos[:, 3])
@@ -32,26 +30,30 @@ atributos[:, 9] = label_encoder.fit_transform(atributos[:, 9])
 atributos[:, 13] = label_encoder.fit_transform(atributos[:, 13])
 classe = label_encoder.fit_transform(classe)
 
-# OPCIONAL : USANDO COLUMNTRANSFORMER
-'''
+# ONE_HOT_ENCODER
 one_hot_encoder = OneHotEncoder()
 column_tranformer = ColumnTransformer([('one_hot_encoder', OneHotEncoder(), [1, 3, 5, 6, 7, 8, 9, 13])],remainder='passthrough')
 atributos = column_tranformer.fit_transform(atributos).toarray()
-'''
 
-#Aplicando escalonamento nos valores
-#scaler = StandardScaler()
-#atributos = scaler.fit_transform(atributos)
-previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(atributos, classe, test_size=0.15, random_state=0)
+# STANDARD_SCALER
+scaler = StandardScaler()
+atributos = scaler.fit_transform(atributos)
 
-
-classificador = RandomForestClassifier(n_estimators=40, criterion= 'entropy', random_state = 0)
-classificador.fit(previsores_treinamento, classe_treinamento)
-previsoes = classificador.predict(previsores_teste)
-
-# Calculando a precisão e gerando matriz de confusão
-taxa_precisao = accuracy_score(classe_teste, previsoes)
-matriz = confusion_matrix(classe_teste, previsoes)
-
-
-print(f'A taxa de precisão alcançada foi de aproximadamente{round(taxa_precisao*100)}%')
+# STRATIFIEDKFOLD
+skfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=0)
+resultados = []
+matrizes = []
+for indice_treinamento, indice_teste in skfold.split(atributos,
+                                                    np.zeros(shape=(atributos.shape[0], 1))):
+   classificador = RandomForestClassifier(n_estimators=40, criterion='entropy', random_state=0)
+   classificador.fit(atributos[indice_treinamento], classe[indice_treinamento])     
+   previsoes = classificador.predict(atributos[indice_teste])
+   precisao = accuracy_score(classe[indice_teste], previsoes)
+   matrizes.append(confusion_matrix(classe[indice_teste], previsoes))
+   resultados.append(precisao)
+   
+matriz_final = np.mean(matrizes, axis=0)
+resultados = np.asarray(resultados)
+   
+print(f'Média = {resultados.mean()}')
+print(f'Desvio Padrão = {resultados.std()}')
